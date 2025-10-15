0ea23bde97463b52bc3f9defb7c6d69b2f68f5a0
maribellq <mb7736562@gmail.com>
2025-10-15 19:27:31 +0300

    Add files via upload

---
+++ джава.java
@@ -0,0 +1,10 @@
+//Список на Java
+ArrayList<String> list = new ArrayList<>();
+list.add("Солнце");
+list.add("Луна");
+
+//Cтек на Java
+MyStack<Integer> stack = new MyStack<>().
+stack.push(11);
+stack.push(22);
+stack.push(33);
